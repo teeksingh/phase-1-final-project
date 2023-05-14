f6688763d2f337af2de3b7a4946f889377d24f1d
teeksingh <teeksingh@gmail.com>
2023-05-14 04:56:34 -0500

    Beginning array iteration code next

--- src/script.js
+++ src/script.js
@@ -3,10 +3,10 @@
       fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
         .then((response) => response.json())
         .then((data) => {
-          console.log(data);
+          console.log(data.drinks[0]);
         });
         return fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
     }
 
-    const randomButton = document.querySelector('.button')
-    randomButton.addEventListener('click', function(){alert('I was clicked')})
+    // const randomButton = document.querySelector('.button')
+    // randomButton.addEventListener('click', function(){alert('I was clicked')})
