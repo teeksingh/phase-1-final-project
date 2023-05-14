1b3c8c31cac9646c44d41e40af61af2a48b92c6a
teeksingh <teeksingh@gmail.com>
2023-05-14 07:09:51 -0500

    test

--- src/script.js
+++ src/script.js
@@ -6,15 +6,18 @@
     .then((response) => response.json())
     .then((data) => {
       let myDrink = data.drinks[0]
-      console.log(myDrink)
+      // console.log(myDrink)
+      filterArray(myDrink)
       // console.log(myDrink.strDrink);
       // console.log(myDrink.strDrinkThumb);
       // console.log(myDrink.strInstructions);
       // console.log(myDrink.strIngredient);
     });
-
 }
-          
+    function filterArray(myDrink){
+     
+    }
     
     // const randomButton = document.querySelector('.button')
     // randomButton.addEventListener('click', function(){alert('I was clicked')})
+    git rm --cached phase-1-final-project
