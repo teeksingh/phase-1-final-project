0f46fd37bdc41f07570147692d447bb969e3c8d6
teeksingh <teeksingh@gmail.com>
2023-05-14 07:02:05 -0500

    filterArray method

--- src/script.js
+++ src/script.js
@@ -1,12 +1,20 @@
 
-  function getRandomDrink() {
-      fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data.drinks[0]);
-        });
-        return fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
-    }
 
+  
+ function getRandomDrink() {
+  fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+    .then((response) => response.json())
+    .then((data) => {
+      let myDrink = data.drinks[0]
+      console.log(myDrink)
+      // console.log(myDrink.strDrink);
+      // console.log(myDrink.strDrinkThumb);
+      // console.log(myDrink.strInstructions);
+      // console.log(myDrink.strIngredient);
+    });
+
+}
+          
+    
     // const randomButton = document.querySelector('.button')
     // randomButton.addEventListener('click', function(){alert('I was clicked')})
