fb7bed7da3fdc6f1b6e9d2e8d93732dd555dde87
Prateek <teeksingh@gmail.com>
2023-05-20 01:57:18 -0500

    Finally got MVP working with 2 event listeners and forEach array iteration

--- src/script.js
+++ src/script.js
@@ -1,11 +1,53 @@
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("The DOM has loaded");
+  console.log("The DOM has loaded")
+  grabButtons()
 });
 
-const baseSearchUrl = 'https://www.thecocktaildb.com/api/json/v1/1/'
+const baseSearchUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php'
 const randomSearchUrl = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
 
 const drinkCardCollection = document.querySelector('#drinkCollection')
+
+const getDrinks = (childNode) => {
+  const url = baseSearchUrl + '?i=' + childNode.id
+    fetch(url)
+    .then(res => res.json())
+    .then(hash => hash['drinks'].forEach(drink => renderDrink(drink))) 
+};
+const renderDrink = (drink) => {
+  const drinkCard = document.createElement('div')
+  drinkCard.className = "card"
+
+  const drinkName = document.createElement('h2')
+  drinkName.innerText = drink.strDrink
+
+  const drinkImage = document.createElement('img')
+  drinkImage.src = drink.strDrinkThumb
+  drinkImage.className = "drink-image"
+
+
+  drinkCard.append(drinkName, drinkImage)
+
+  drinkCollection.append(drinkCard)
+
+};
+const grabButtons = () => {
+  document.querySelector('#pop-buttons').childNodes.forEach(childNode => { 
+      childNode.addEventListener('click', function(){
+          removeDrinks()
+          getDrinks(childNode)
+      })
+  })
+};
+
+const removeDrinks = () => {
+  drinkCollection.textContent = ""
+  while (drinkCollection.firstChild) {
+    drinkCollection.removeChild(drinkCollection.lastChild);
+  }
+}
+
+
 
 const button = document.getElementById("button")
 button.addEventListener("click", getRandomDrink)
@@ -18,35 +60,25 @@ function getRandomDrink() {
     .then((response) => response.json())
     .then((data) => {
       const myDrink = data.drinks[0]
-     printOutput(myDrink)
+     removeResults()
+      printOutput(myDrink)
+    
     });
 }
+
 
 function printOutput(myDrink){
       const image = document.createElement("img")
       image.src= myDrink.strDrinkThumb
       document.querySelector('#drinkImage').appendChild(image)
-
       const name = document.getElementById('drinkName').textContent = myDrink.strDrink
-
       const glassType = document.getElementById('drinkGlassType').textContent = myDrink.strGlass
-
-      for (const key in myDrink) {
-        if (myDrink[key] !== null)
-        console.log(myDrink.key)
-      }
-      const measure = document.getElementById('drinkMeasure').textContent = myDrink.strMeasure1
-
-      const Ingredients = document.getElementById('drinkIngredients').textContent = myDrink.strIngredient1
-
+      const measure = document.getElementById('drinkMeasure').textContent = myDrink.strMeasure1 + myDrink.strIngredient1
+      const Ingredients = document.getElementById('drinkIngredients').textContent = myDrink.strMeasure2 + myDrink.strIngredient2 + myDrink.strMeasure3 + myDrink.strIngredient3
       const Instructions = document.getElementById('drinkInstructions').textContent = myDrink.strInstructions
       // console.log(myDrink)
     }
 
-  function printOutput2(myDrink)
-  {
-    
-  }
 function removeResults(){
       const removeName = document.getElementById('drinkName')
       removeName.textContent = ""
@@ -67,13 +99,3 @@ function removeResults(){
       Instructions2.textContent = ""
 
     }
-  
-function searchByIngredient() {
-  const url = baseSearchUrl + '?i=' + userInput
-  fetch(url)
-  .then ((response) => response.json())
-  .then((json) => {
-    console.log(json)
-  })
-}
-
